c603d6714c18096267a7db2cedd7034c56d657ef
Arunlal <arun.lal@lilacinfotech.com>
2022-10-14 01:51:32 +0530

    Matrimony v1

---
+++ assets/js/custom.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+    //feather icon
+    feather.replace()
+    
+
+
+
+// add class
+    $('.open-menu').click(function(){
+        $('nav').addClass('open');
+    });
+    $('.close-menu, nav a').click(function(){
+        $('nav').removeClass('open');
+    });
+
+
+
+
+
+}); // doc rdy close
